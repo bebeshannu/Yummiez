63f9f9c23ef334dc2fffefe718bb459fd285ee85
bebeshannu <shainajyothica27@gmail.com>
2023-05-07 14:56:33 +0530

    Categories Added

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,6 +2,8 @@ import { View, Text, SafeAreaView, Image, ViewComponent, TextInput, ScrollView }
 import React, { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { UserIcon, ChevronDownIcon, MagnifyingGlassCircleIcon, AdjustmentsVerticalIcon } from "react-native-heroicons/outline";
+import Categories from '../components/Categories';
+
 
 const HomeScreen = () => {
 
@@ -13,45 +15,52 @@ const HomeScreen = () => {
         });
     }, []);
   return (
-    <SafeAreaView className="display:flex bg-white pl-2 pt-7">
-      <Text className="text-black-500 ">
+    <SafeAreaView className=" bg-white  pt-7">
+      
         {/* Header */}
-        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+        <View className=' flex-row pb-3 items-center mx-3 space-x-2'>
             <Image
             source={{
                 uri: 'https://links.papareact.com/wru',
             }}
             className ="h-7 w-7 bg-gray-300 p-4 rounded-full"
             />
-            <View className ='flex-1'>
+            <View className ='flex-1 '>
                 <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
                 <Text className="font-bold text-xl">Current Location
                 <ChevronDownIcon size={20} color="#00CCBB"/>    
-                <Text className="font-bold text-white text-xs">Deeeeeeeeeeeeeeeeeee</Text>
+                {/* <Text className="font-bold text-white text-xs">Deeeeeeeeeeeeeeeeee</Text> */}
                 
                 </Text>
 
             </View >
-            <UserIcon className="" size={33} color="#00CCBB" />
+            <UserIcon size={33} color="#00CCBB" />
         </View>
         {/* Search */}
-        <View className=" flex flex-row items-center space-x-2 pb-3 mx-4">
+        <View className=" flex-row items-center space-x-2 pb-3 mx-3">
           <View className="flex-1 flex-row space-x-2 rounded-full bg-gray-200 p-1 ">
             <MagnifyingGlassCircleIcon  color='gray' size={25} />
             <TextInput  placeholder='Restaurants and cuisines'
             keyboardType='default'
             
             />
-             <Text className="font-bold text-gray-200 text-xs">Deeeeeeeeeeeeeeeeeee</Text> 
+             {/* <Text className="font-bold text-gray-200 text-xs">Deeeeeeeeeeeeeeeeeee</Text>  */}
             
           </View>
           <AdjustmentsVerticalIcon color='#00CCBB' />
         </View>
         {/* Body */}
-        <ScrollView>
-          
+        <ScrollView className="bg-gray-100"
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}
+        >
+          {/* Categories */}
+          <Categories/>
+
+          {/* Featured rows */}
         </ScrollView>
-      </Text>
+      
     </SafeAreaView>
   )
 }
