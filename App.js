c7ec5c120618f25bd5ea64ebaaf96cad66a8fb38
bebeshannu <shainajyothica27@gmail.com>
2023-05-11 02:41:29 +0530

    Restaurant Screen Addes

--- App.js
+++ App.js
@@ -6,6 +6,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import 'react-native-url-polyfill/auto';
+import RestaurantScreen from './screens/RestaurantScreen';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 
 
@@ -14,11 +17,15 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
+      <Provider store={store}>
+
     <TailwindProvider>
     <Stack.Navigator>
     <Stack.Screen name="Home" component={HomeScreen} />
+    <Stack.Screen name="Restaurant" component={RestaurantScreen} />
     </Stack.Navigator>
     </TailwindProvider>
+    </Provider>
     </NavigationContainer>
   );
 }
--- components/RestaurantCard.js
+++ components/RestaurantCard.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { StarIcon } from 'react-native-heroicons/solid';
 import { MapPinIcon } from 'react-native-heroicons/outline';
 import { urlFor } from '../sanity';
+import { useNavigation } from '@react-navigation/native';
 
 const RestaurantCard = ({
     id,
@@ -16,8 +17,24 @@ const RestaurantCard = ({
     long,
     lat,
 }) => {
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity className="bg-white mr-3 shadow-xl ">
+    <TouchableOpacity
+    onPress={()=>{
+      navigation.navigate("Restaurant",{
+        id,
+        imgUrl,
+        title,
+        rating,
+        genre,
+        address,
+        short_description,
+        dishes,
+        long,
+        lat,
+      })
+    }}
+    className="bg-white mr-3 shadow-xl ">
       <Image
       source={{
         uri: urlFor(imgUrl).url(),
@@ -33,7 +50,7 @@ const RestaurantCard = ({
             </Text>
         </View>
         <View className="flex-row items-center space-x-1">
-            <MapPinIcon className="rounded-full" color='gray' opacity={0.4} size={21}/>
+            <MapPinIcon className="rounded-full" color='red' opacity={0.4} size={20}/>
             <Text className=" text-xs text-gray-500">Nearby • {address}</Text>
         </View>
       </View>
